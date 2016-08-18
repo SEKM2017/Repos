30b72c1efef769ae0bd8291ad31a1cc088e79645
Fongue <hadriendurbois.taghuofongue@rewe-group.com>
2016-08-18 14:21:57 +0200

    Initial checkin

---
+++ TutorialGit/src/git/App.java
@@ -0,0 +1,13 @@
+package git;
+
+public class App {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello meine Studenten");
+
+		System.out.println("Hello");
+
+	}
+
+}
