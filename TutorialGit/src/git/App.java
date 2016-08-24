c65c64f105dc2f0cc9a7db8a09f0947c1178a879
Fongue <HadrienDurbois.TaghuoFongue@rewe-group.com>
2016-08-24 09:40:18 +0200

    24.08

--- TutorialGit/src/git/App.java
+++ TutorialGit/src/git/App.java
@@ -6,7 +6,7 @@ public class App {
 		// TODO Auto-generated method stub
 		System.out.println("Hello meine Studenten");
 
-		System.out.println("Hello REWE Portal  ");
+		System.out.println("Hello REWE Portal für MA");
 
 	}
 
