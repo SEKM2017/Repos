f8d13c459a8ad9fc9c22157ced04334734f07945
Taghuo <taghuoharien@yahoo.de>
2016-08-24 16:46:00 +0200

    Update App.java

--- TutorialGit/src/git/App.java
+++ TutorialGit/src/git/App.java
@@ -6,7 +6,7 @@ public class App {
 		// TODO Auto-generated method stub
 		System.out.println("Hello meine Studenten");
 
-		System.out.println("Ci-Admin Projekt muss fertig gemacht werden");
+		System.out.println("Ci-Admin Projekt ist nicht schwer");
 
 	}
 
